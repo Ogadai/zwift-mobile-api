493c61d146b1675d9308ebff829d5929ce147aa6
Andy <andy.lee.surfer@gmail.com>
2017-04-05 11:22:32 +0100

    Added RiderAttributes message and ability to POST requests
    RiderAttributes can be used to post a ride-on

--- src/Request.js
+++ src/Request.js
@@ -24,6 +24,14 @@ class Request {
     }
 
     request(url, acceptType, responseType) {
+        return this.send(url, 'get', null, acceptType, responseType)
+    }
+
+    post(url, data, acceptType, responseType) {
+        return this.send(url, 'post', data, acceptType, responseType)
+    }
+
+    send(url, method, data, acceptType, responseType) {
         return _queue.add().then(() => {
             return this.tokenFn().then(token => {
                 return axios.get(url, {
@@ -35,10 +43,27 @@ class Request {
                     responseType
                 })
                 .then(function (response) {
-                    return response.data
+                    return response.data;
                 })
-            })
-        })
+                return axios(Object.assign(
+                        { method, url, data },
+                        this.config(acceptType, responseType, token)))
+                    .then(function (response) {
+                        return response.data;
+                    });
+            });
+        });
+    }
+
+    config(acceptType, responseType, token) {
+        return {
+            baseURL: 'https://us-or-rly101.zwift.com',
+            headers: Object.assign({}, DEFAULT_HEADERS, {
+                "Accept": acceptType,
+                "Authorization": "Bearer " + token
+            }),
+            responseType
+        };
     }
 }
 
