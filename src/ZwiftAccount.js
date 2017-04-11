a86242a56b8fe50b56557e42fbca4c3dbb549288
Christian Wiedmann <christian@mobilehealthconsumer.com>
2017-04-11 13:32:51 -0700

    Add support to ZwiftAccount to authenticate using refresh token. This token can be stored and reused (currently for thirty days). Passing a refresh token in as the third parameter to the ZwiftAccount constructor will make it use refresh token authentication instead of password authentication.

--- src/ZwiftAccount.js
+++ src/ZwiftAccount.js
@@ -7,12 +7,16 @@ const Activity = require('./Activity')
 const TOKEN_REFRESH_MS = 10 * 60 * 1000
 
 class ZwiftAccount {
-    constructor(username, password) {
+    constructor(username, password, refreshToken = null) {
         this.username = username
         this.password = password
 
         this.tokenPromise = null
         this.tokenDate = null
+
+        this.refreshToken = refreshToken
+        this.refreshTokenExpiration = 0
+        this.accessTokenExpiration = 0
 
         this.getAccessToken = this.getAccessToken.bind(this)
     }
@@ -34,11 +38,31 @@ class ZwiftAccount {
     }
 
     getAccessToken() {
-        if (!this.tokenPromise || (new Date().getTime() - this.tokenDate.getTime() > TOKEN_REFRESH_MS)) {
-            this.tokenDate = new Date()
-            this.tokenPromise = getAccessToken(this.username, this.password)
+        const now = new Date()
+        if (!this.tokenPromise || (now.getTime() > this.accessTokenExpiration)) {
+            this.getNewTokens()
         }
 
+        return this.tokenPromise
+    }
+
+    getRefreshToken() {
+        return this.getAccessToken().then(() => {
+            return this.refreshToken, this.refreshTokenExpiration
+        })
+    }
+
+    getNewTokens() {
+        const now = new Date()
+        this.tokenDate = now
+        this.tokenPromise = getAccessToken(this.username, this.password, this.refreshToken).then((response) => {
+            const now = new Date()
+            this.accessTokenExpiration = now.getTime() + ((response.data.expires_in - 5) * 1000)
+            this.refreshTokenExpiration  = now.getTime() + ((response.data.refresh_expires_in - 5) * 1000)
+            this.refreshToken = response.data.refresh_token
+            this.accessToken = response.data.access_token
+            return response.data.access_token, this.accessTokenExpiration
+        })
         return this.tokenPromise
     }
 }
--- src/getAccessToken.js
+++ src/getAccessToken.js
@@ -1,19 +1,25 @@
 ﻿const axios = require('axios')
 const qs = require('qs')
 
-module.exports = function getAccessToken(username, password) {
-    const data = {
-        "client_id": "Zwift_Mobile_Link",
-        "username": username,
-        "password": password,
-        "grant_type": "password",
+module.exports = function getAccessToken(username, password, refreshToken = null) {
+    let data
+    if (refreshToken) {
+        data = {
+            "client_id": "Zwift_Mobile_Link",
+            "refresh_token": refreshToken,
+            "grant_type": "refresh_token",
+        }
+    } else {
+        data = {
+            "client_id": "Zwift_Mobile_Link",
+            "username": username,
+            "password": password,
+            "grant_type": "password",
+        }
     }
 
     return axios.post('/auth/realms/zwift/tokens/access/codes',
         qs.stringify(data), {
             baseURL: 'https://secure.zwift.com'
-        })
-        .then(function (response) {
-            return response.data.access_token
         })
 }
