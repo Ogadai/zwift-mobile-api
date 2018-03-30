edfdfd24811e385360f9f03d54c001c54094e51e
Andy <andy.lee.surfer@gmail.com>
2018-03-30 15:40:41 +0100

    Tweak mapping transform

--- src/mapLatLong.js
+++ src/mapLatLong.js
@@ -1,8 +1,8 @@
 ﻿const maps = {
   1: {
     toXY: (lat, long) => ({
-      x: (lat - 348.3551) * 11050000,
-      y: (long - 166.9529) * 10840000
+      x: Math.round((lat - 348.35518) * 11050000),
+      y: Math.round((long - 166.95292) * 10920000)
     }),
     offset: {
       lat: 360,
@@ -11,8 +11,8 @@
   },
   2: {
     toXY: (lat, long) => ({
-      x: (lat - 37.54303) * 11080000,
-      y: (long - 282.56252) * 8790000
+      x: Math.round((lat - 37.54303) * 11080000),
+      y: Math.round((long - 282.56252) * 8790000)
     }),
     offset: {
       lat: 0,
@@ -21,8 +21,8 @@
   },
   3: {
     toXY: (lat, long) => ({
-      x: (long - 359.8321) * 6940000,
-      y: -(lat - 51.5017) * 11120000
+      x: Math.round((long - 359.8321) * 6940000),
+      y: -Math.round((lat - 51.50175) * 11130000)
     }),
     offset: {
       lat: 0,
